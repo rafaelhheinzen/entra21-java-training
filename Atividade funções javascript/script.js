cceedaa05be3b1ff5a9f4ea88fbd2d7f48604d23
Rafael Henrique <rafa.zqn@gmail.com>
2026-05-26 14:07:54 -0300

    Add files via upload

---
+++ Atividade funções javascript/script.js
@@ -0,0 +1,25 @@
+        const result = document.getElementById("result")
+
+
+
+
+        //Sum function
+        const sumBtn = document.getElementById("sumBtn")
+
+
+
+        sumBtn.addEventListener("click", ()=>{
+            let number1 = document.getElementById("number1").value
+            let number2 = document.getElementById("number2").value
+
+            n1 = Number(number1)
+            n2 = Number(number2)
+            add(n1, n2)
+        })
+
+        function add(n1, n2){
+            let numberResult = n1 + n2;
+            result.innerHTML = numberResult
+        }
+
+
