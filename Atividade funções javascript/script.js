22a34c9e5b51a406811f959fd7f2534015e5d6b2
Rafael Henrique <rafa.zqn@gmail.com>
2026-05-26 15:08:13 -0300

    Update script.js

--- Atividade funções javascript/script.js
+++ Atividade funções javascript/script.js
@@ -1,11 +1,12 @@
     const result = document.getElementById("result")
-    const sumBtn = document.getElementById("sumBtn")
+    const subtractResult = document.getElementById("subtractResult")
 
 
     //Sum Button function
+    const sumBtn = document.getElementById("sumBtn")
     sumBtn.addEventListener("click", ()=>{
-            let number1 = document.getElementById("number1").value
-            let number2 = document.getElementById("number2").value
+            let number1 = document.getElementById("sumNumber1").value
+            let number2 = document.getElementById("sumNumber2").value
 
             n1 = Number(number1)
             n2 = Number(number2)
@@ -20,3 +21,22 @@
     }
 
 
+
+
+    //Subtract Button function
+    const subtractBtn = document.getElementById("subtractBtn")
+    subtractBtn.addEventListener("click", ()=>{
+        let number1 = document.getElementById("subtractNumber1").value
+        let number2 = document.getElementById("subtractNumber2").value
+
+        n1 = Number(number1)
+        n2 = Number(number2)
+        subtract(n1, n2)
+
+    })
+
+
+    function subtract(n1, n2){
+        let numberResult = n1 - n2;
+        subtractResult.innerHTML = numberResult
+    }
