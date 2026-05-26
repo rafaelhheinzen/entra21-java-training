db70034a62a4e39ee29b348ca5df241746da7b3e
Rafael Henrique <rafa.zqn@gmail.com>
2026-05-26 14:43:47 -0300

    Update script.js

--- Atividade funções javascript/script.js
+++ Atividade funções javascript/script.js
@@ -1,25 +1,22 @@
-        const result = document.getElementById("result")
+    const result = document.getElementById("result")
+    const sumBtn = document.getElementById("sumBtn")
 
 
-
-
-        //Sum function
-        const sumBtn = document.getElementById("sumBtn")
-
-
-
-        sumBtn.addEventListener("click", ()=>{
+    //Sum Button function
+    sumBtn.addEventListener("click", ()=>{
             let number1 = document.getElementById("number1").value
             let number2 = document.getElementById("number2").value
 
             n1 = Number(number1)
             n2 = Number(number2)
             add(n1, n2)
-        })
+    })
 
-        function add(n1, n2){
-            let numberResult = n1 + n2;
-            result.innerHTML = numberResult
-        }
+
+    //Sum function
+    function add(n1, n2){
+        let numberResult = n1 + n2;
+        result.innerHTML = numberResult
+    }
 
 
