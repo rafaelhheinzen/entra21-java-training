a578127fdebc8bb9cb3575af55d0be2225b2a109
Rafael Henrique <rafa.zqn@gmail.com>
2026-06-30 16:33:10 -0300

    Add unidadeMedida field to Produto class

--- Java - OOP/Produto.java
+++ Java - OOP/Produto.java
@@ -6,10 +6,15 @@
         private double preco;
         private int estoque;
         private int id;
+        private String unidadeMedida;
 
         // Getters
         public String getNome() {
             return this.nome;
+        }
+
+        public String getUnidadeMedida() {
+            return this.unidadeMedida;
         }
 
         public double getPreco() {
@@ -37,16 +42,19 @@
             this.estoque = estoque;
         }
 
+        void setUnidadeMedida(String unidadeMedida){
+            this.unidadeMedida = unidadeMedida;
+        }
 
 
 
-
-        Produto(String nome, double preco, int estoque) {
+        Produto(String nome, double preco, int estoque, String unidadeMedida) {
             this.id = contadorId;
             contadorId++;
 
             this.nome = nome;
             this.preco = preco;
             this.estoque = estoque;
+            this.unidadeMedida = unidadeMedida;
         }
     }
