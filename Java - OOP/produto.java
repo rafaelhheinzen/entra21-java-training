fb71a6772399835344667fb56a59d99c9b048624
Rafael Henrique <rafa.zqn@gmail.com>
2026-06-30 16:18:17 -0300

    Create produto.java

---
+++ Java - OOP/produto.java
@@ -0,0 +1,52 @@
+public class Produto {
+
+    private static int contadorId = 1;
+
+    private String nome;
+    private double preco;
+    private int estoque;
+    private int id;
+
+    // Getters
+    public String getNome() {
+        return this.nome;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public int getEstoque() {
+        return this.estoque;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    //Setters
+    void setNome(String nome){
+        this.nome = nome;
+    }
+
+    void setPreco(double preco){
+        this.preco = preco;
+    }
+
+    void setEstoque(int estoque){
+        this.estoque = estoque;
+    }
+
+
+
+
+
+    Produto(String nome, double preco, int estoque) {
+        this.id = contadorId;
+        contadorId++;
+
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+}
