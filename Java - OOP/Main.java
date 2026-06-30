4307dffdf0ad4f00206549aaa493b3c1958de019
Rafael Henrique <rafa.zqn@gmail.com>
2026-06-30 16:18:31 -0300

    Update Main.java

--- Java - OOP/Main.java
+++ Java - OOP/Main.java
@@ -1,36 +1,8 @@
-public class Main{
-
-public static void main(String[] args) {
-    //Setting the first cow;
-    Cow cow1 = new Cow("Belly", "Holstein", 500.0, 5);
-
-    //Executing showStats method;
-    cow1.showStats();
-
-}
-
-}
-
-
-class Cow{
-    String name;
-    String race;
-    double weight;
-    int age;
-
-
-    void showStats(){
-        System.out.println("Cow analyzer");
-        System.out.println("Name: "+this.name);
-        System.out.println("Race: "+this.race);
-        System.out.println("Weight: "+this.weight);
-        System.out.println("Age: "+this.age);
+public class Main {
+    
+    public static void main(String[] args) {
+        Mercado mercado = new Mercado();
+        mercado.repetir();
     }
 
-    Cow(String name, String race, double weight, int age){
-        this.name = name;
-        this.race = race;
-        this.weight = weight;
-        this.age = age;
-    }
 }
